0ea690b24bb45766de23005bd57a0dda1de2d05b
Arthur Machado <machadoparthur1@gmail.com>
2024-09-20 10:17:26 -0300

    feat: explore oop polymorphism and encapsulation concepts

--- poo_concepts.py
+++ poo_concepts.py
@@ -1,4 +1,3 @@
-# Inheritance
 print('\nExample of Inheritance:')
 class Animal:
   def __init__(self, name) -> None:
@@ -18,3 +17,37 @@ class Dog(Animal):
 class Cat(Animal):
   def speak(self):
     return 'Miau'
+
+dog = Dog(name='Cat')
+cat = Cat(name='Dog')
+
+print('\nExample of Polymorphism:')
+animals = [dog, cat]
+
+for animal in animals:
+  print(f'The animal {animal.name} speaks {animal.speak()}')
+
+print('\nExample of Encapsulation.')
+class BankAccount:
+  def __init__(self, balance) -> None:
+    self.__balance = balance # private property
+  
+  def deposit(self, value):
+    if value > 0:
+      self.__balance += value
+  
+  def withdraw(self, value):
+    if value > 0 and value <= self.__balance:
+      self.__balance -= value
+    
+  def get_balance(self):
+    return self.__balance
+  
+account = BankAccount(1000)
+print(f'Balance account: {account.get_balance()}')
+account.deposit(500)
+print(f'Balance account: {account.get_balance()}')
+account.deposit(-500)
+print(f'Balance account: {account.get_balance()}')
+account.withdraw(200)
+print(f'Balance account: {account.get_balance()}')
